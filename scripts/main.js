b7f119bc3d7ec476190a41d8236a7eb53c8ee0d5
Oleksandr07 <Oleksandr07@mail.ru>
2016-08-22 16:35:05 +0300

    designers.html (tablet)

--- scripts/main.js
+++ scripts/main.js
@@ -225,7 +225,7 @@ $(function() {
 	// });
 		// init .example-slider slick-slider
 	$(".example-slider").slick({
-		autoplay: true,
+		//autoplay: true,
 		autoplaySpeed: 3000,
 		speed: 500,
 		slidesToShow: 1,
@@ -846,8 +846,13 @@ function topPromoW() {
 	if($(".top-promo").length) {
 		var cbW = $(".center-block").width();
 		var winW = $(window).width();
-		var targetW = winW - (winW - cbW) / 2 - 174;
-		console.log(winW - (winW - cbW) / 2);
+		if (winW > 1439) {
+			var targetW = winW - (winW - cbW) / 2 - 174;
+		} 
+		else {
+			var targetW = winW - (winW - cbW) / 2 - 50;
+		}
+		
 		$(".top-promo").width(targetW);
 	}
 }
